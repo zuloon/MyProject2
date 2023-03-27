e80adcff5c9a96b5497ef5aab54aa405a3e45c7d
Kasidatep LX <lx@kasidate.me>
2023-03-27 14:23:44 +0700

    'pussy'

---
+++ myproject23/Myproject23.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package myproject23;
+
+/**
+ *
+ * @author Student
+ */
+public class Myproject23 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
